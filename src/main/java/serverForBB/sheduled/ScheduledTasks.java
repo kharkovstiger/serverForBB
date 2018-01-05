1e7ac779ea78d3ed1fb97e2cb1a3ace90f200809
kharkovstiger <kharkovs.tiger@gmail.com>
2018-01-05 12:23:44 +0200

    scheduling test

--- src/main/java/serverForBB/sheduled/ScheduledTasks.java
+++ src/main/java/serverForBB/sheduled/ScheduledTasks.java
@@ -18,11 +18,11 @@ public class ScheduledTasks {
     }
 
 //    @Scheduled(cron = "0 0 2 ? * 2")
-    @Scheduled(cron = "0 48 8 ? * *")
+    @Scheduled(cron = "0 27 10 ? * *")
     public void addGames(){
 //        Integer maxId= Integer.valueOf(gameService.getMaxId());
         System.err.println("Begin to add new games");
-        Integer maxId=10105;
+        Integer maxId=10970;
         boolean flag=true;
         while (flag){
             try {
